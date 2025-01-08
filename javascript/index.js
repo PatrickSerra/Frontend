9deb971f9e42130672a0591553c2ee269de3b9fc
Patrick Serra <balius_dagon@hotmail.com>
2025-01-08 15:14:34 -0400

    coomit

---
+++ javascript/index.js
@@ -0,0 +1,40 @@
+let str = 'Hi';
+let gretting = "Hello";
+let message =  `"I'm good". She Said`;
+
+str = 'I\'m a string \\ break';
+console.log(str);
+
+let greeting = 'Good Morning!';
+console.log(greeting.length);
+
+console.log(str[0]);
+console.log(str[str.length - 1]);
+
+let name = 'John';
+let welcome = 'Hello ' + name;
+console.log(welcome);
+
+let className = 'btn';
+className += ' btn-primary';
+className += ' none'
+
+console.log(className);
+
+
+let status = false;
+let string = status.toString();
+console.log(string);
+
+let back = Boolean(str);
+console.log(back);
+
+let number = 2;
+let str3 = number.toString()
+console.log(typeof number);
+console.log(typeof str3);
+console.log('a' < 'B');
+
+
+
+
