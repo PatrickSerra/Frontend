67b8fc50852c83cec39c686b5dd7db4daee3ae02
Patrick Serra <balius_dagon@hotmail.com>
2025-01-12 17:50:39 -0400

    commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,65 +1,94 @@
-function say(messsage) {
-    console.log(messsage);
-}
+// function Person(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+// }
 
-let result = say('Hello');
-console.log('Result:', result);
+// Person.prototype.getFullname = function() {
+//     return this.firstName + ' ' + this.lastName;
+// }
+
+// let p1 = new Person("John", "Doe");
+// let p2 = new Person('Jane', 'Doe');
+
+// console.log(p1.getFullname());
+// console.log(p2.getFullname());
+// console.log(Object.getPrototypeOf(p1));
 
 
-function add(a, b) {
-    return a + b;
-}
 
-let sum = add(10, 20);
-console.log('Sum:', sum);
+// class Person {
+//     constructor(firstName, lastName) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+    
+//     getFullname() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
 
+// let p1 = new Person("John", "Doe");
+// let p2 = new Person('Jane', 'Doe');
 
-function  compare(a, b) {
-    if (a > b) {
-        return -1;
-    } else if (a < b) {
-        return 1;
+// console.log(p1.getFullname());
+// console.log(p2.getFullname());
+// console.log(Object.getPrototypeOf(p1));
+
+let person = {
+    name: "John Doe",
+    greet() {
+        return "Hi, I'm this.name";
     }
-
-    return 0;
 }
 
-function add() {
-    let sum = 0;
-    for(let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
+console.log(person.toString());
+console.log(person.__proto__ === Object.prototype);
+
+let teacher = Object.create(person);
+
+teacher.name = 'Jane Doe'
+teacher.teach = function (subject) {
+    return "I can teach " + subject;
+}
+
+console.log(Object.getPrototypeOf(teacher) === person);
+
+
+let counter = {
+    counter: 0,
+    next: function () {
+        return ++this.count;
     }
-
-    return sum;
 }
 
-console.log(add(1, 2));
-console.log(add(1, 2, 3, 4, 5));
+counter.next();
 
-
- 
-
+this.color = 'Red';
 
 
 
 
+function show() {
+    console.log(this);
+    
+}
+
+show()
+
+let car = {
+    brand: "Honda",
+    getBrand: function () {
+        return this.brand;
+    }
+}
+
+console.log(car.getBrand());
+
+let brand = car.getBrand.bind(car);
+
+console.log(brand());
 
 
+const canFetch = typeof globalThis.fetch === 'function';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(canFetch);
