ed22dcbf840cdab21bc7d8e4b84e8d6556fdd250
Patrick Serra <balius_dagon@hotmail.com>
2025-02-19 14:20:52 -0400

    commit

---
+++ javascript/react/fundamentos-react-app/src/components/base/First.jsx
@@ -0,0 +1,6 @@
+export default function First() {
+    const msg = 'Seja bem vindo(a)'
+    return(
+        <h1>Primeiro componente {msg}</h1>    
+    )    
+}
--- javascript/react/fundamentos-react-app/src/main.jsx
+++ javascript/react/fundamentos-react-app/src/main.jsx
@@ -1,7 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import First from './components/base/First'
 
 createRoot(document.getElementById('root')).render(
-  "Ola mundo"
-)
+  
+  <StrictMode>
+    <First></First>
+    <First></First>
+    <First></First>
+    {/* <App /> */}
+  </StrictMode>
+);
